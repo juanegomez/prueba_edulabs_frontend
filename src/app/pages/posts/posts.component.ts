dc5cc19cf193b86e5b3d56cfbab2bf4b5b6455ca
juanegomez <juanegomez1022@gmail.com>
2024-11-16 19:50:28 -0500

    Se agrega vista y funcionalidad para listar los posts segun la categoria

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppbarComponent } from '../../shared/components/appbar/appbar.component';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [AppbarComponent],
+  imports: [AppbarComponent, HttpClientModule, CommonModule],
   templateUrl: './posts.component.html',
-  styleUrl: './posts.component.css'
+  styleUrls: ['./posts.component.css']
 })
-export default class PostsComponent {
+export default class PostsComponent implements OnInit {
+  private apiUrl = environment.apiUrl;
+  posts: any[] = [];
+  loading: boolean = true;
+  selectedCategory: number = 1;
 
+  constructor(private http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+    this.getPosts(this.selectedCategory);
+  }
+
+  getPosts(categoryId: number): void {
+    const url = `${this.apiUrl}/posts/${categoryId.toString()}`;
+
+    const token = (typeof window !== 'undefined' && localStorage.getItem('authToken')) || '';
+
+    const headers = { Authorization: `Bearer ${token}` };
+
+    this.loading = true;
+
+    this.http.get(url, { headers }).subscribe({
+      next: (data: any) => {
+        this.posts = data.data;
+        console.log(this.posts);
+        
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+
+  onCategoryChange(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    this.selectedCategory = parseInt(selectElement.value, 10);
+    this.getPosts(this.selectedCategory);
+  }
 }
